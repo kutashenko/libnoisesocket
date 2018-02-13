2f78ff71b754e9ad3742fc0f5ce6aaad69f78bf6
Roman Kutashenko <kutashenko@gmail.com>
2018-02-13 18:36:27 +0200

    Add native libuv client/server

--- examples/client/client.c
+++ examples/client/client.c
@@ -1,5 +1,78 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <uv.h>
 
-int main() {
-    return -1;
+static void on_close(uv_handle_t* handle);
+static void on_connect(uv_connect_t* req, int status);
+static void on_write(uv_write_t* req, int status);
+
+static uv_loop_t *loop;
+
+static void
+alloc_cb (uv_handle_t *handle, size_t size, uv_buf_t *buf) {
+    buf->base = malloc(size);
+    buf->len = size;
+}
+
+static void
+on_close(uv_handle_t *handle) {
+    printf("closed.");
+}
+
+static void
+on_write(uv_write_t *req, int status) {
+    if (status) {
+        fprintf(stderr, "uv_write error: \n");
+        return;
+    }
+    printf("wrote.\n");
+    //uv_close((uv_handle_t*)req->handle, on_close);
+}
+
+static void
+on_read (uv_stream_t *tcp, ssize_t nread, const uv_buf_t *buf) {
+    if (nread >= 0) {
+        //printf("read: %s\n", tcp->data);
+        printf("read: %s\n", buf->base);
+    } else {
+        //we got an EOF
+        uv_close((uv_handle_t*) tcp, on_close);
+    }
+
+    //cargo-culted
+    free(buf->base);
+}
+
+static void
+on_connect (uv_connect_t *connection, int status) {
+    printf("connected.\n");
+
+    uv_stream_t* stream = connection->handle;
+
+    uv_buf_t buffer[] = {
+            {.base = "hello", .len = 5},
+            {.base = "world", .len = 5}
+    };
+
+    uv_write_t request;
+
+    uv_write(&request, stream, buffer, 2, on_write);
+    uv_read_start(stream, alloc_cb, on_read);
+}
+
+int
+main (int argc, char **argv) {
+    loop = uv_default_loop();
+
+    uv_tcp_t socket;
+    uv_tcp_init(loop, &socket);
+    uv_tcp_keepalive(&socket, 1, 60);
+
+    struct sockaddr_in dest;
+    uv_ip4_addr("0.0.0.0", 30000, &dest);
+
+    uv_connect_t connect;
+    uv_tcp_connect(&connect, &socket, (const struct sockaddr*) &dest, on_connect);
+
+    uv_run(loop, UV_RUN_DEFAULT);
 }
--- examples/server/server.c
+++ examples/server/server.c
@@ -1,5 +1,70 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <uv.h>
 
-int main() {
-    return -1;
+uv_loop_t *loop;
+
+static void
+alloc_buffer (uv_handle_t *handle, size_t size, uv_buf_t *buf) {
+    buf->base = malloc(size);
+    buf->len = size;
+}
+
+static void
+echo_write (uv_write_t *req, int status) {
+    if (status == -1) {
+        fprintf(stderr, "Write error!\n");
+    }
+    char *base = (char*) req->data;
+    free(base);
+    free(req);
+}
+
+static void
+echo_read (uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+    if (nread == -1) {
+        fprintf(stderr, "Read error!\n");
+        uv_close((uv_handle_t*)client, NULL);
+        return;
+    }
+
+    uv_write_t *write_req = (uv_write_t*)malloc(sizeof(uv_write_t));
+    write_req->data = (void*)buf->base;
+//  buf->len = nread;
+    uv_write(write_req, client, buf, 1, echo_write);
+}
+
+static void
+on_new_connection(uv_stream_t *server, int status) {
+    if (status == -1) {
+        return;
+    }
+
+    uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
+    uv_tcp_init(loop, client);
+    if (uv_accept(server, (uv_stream_t*) client) == 0) {
+        uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
+    }
+
+    else {
+        uv_close((uv_handle_t*) client, NULL);
+    }
+}
+
+int
+main(int argc, char **argv) {
+    loop = uv_default_loop();
+
+    uv_tcp_t server;
+    uv_tcp_init(loop, &server);
+
+    struct sockaddr_in bind_addr;
+    uv_ip4_addr("0.0.0.0", 30000, &bind_addr);
+    uv_tcp_bind(&server, (const struct sockaddr*) &bind_addr, 0);
+    int r = uv_listen((uv_stream_t*) &server, 128, on_new_connection);
+    if (r) {
+        fprintf(stderr, "Listen error!\n");
+        return 1;
+    }
+    return uv_run(loop, UV_RUN_DEFAULT);
 }
